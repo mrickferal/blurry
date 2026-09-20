5a76d9fd0db5b282bd39bc6d3ef8ea8c81c9e613
aimerickferal <contact@aimericferal.com>
2026-09-20 19:47:12 +0200

    improve blurry.js

--- assets/js/modules/blurry.js
+++ assets/js/modules/blurry.js
@@ -2,69 +2,51 @@ const blurry = {
   init: function () {
     console.log("Hello world, I'm blurry.js 🌫️");
 
-    // crearte an array from the elements
+    // create an array from the elements
     blurry.elements = Array.from(document.querySelectorAll(".luna"));
 
     for (let element of blurry.elements) {
-      // add a listener and a handler on the 'mouseenter' and 'mouseleave' event on each of them
+      // add a listener and a handler on the "mouseenter" and "mouseleave" event on each of them
       element.addEventListener("mouseenter", blurry.handleMouseEnter);
       element.addEventListener("mouseleave", blurry.handleMouseLeave);
     }
   },
   /**
-   * Add a CSS class on the hover element.
+   * Blurs every element except the one being hovered.
    * @param {Event} event
    * @return {void}
    */
   handleMouseEnter: function (event) {
-    console.log("blurry.handleMouseEnter");
+    // console.log("blurry.handleMouseEnter()");
 
-    // get the DOM element form which the event occured
-    let hoverElement = event.currentTarget;
-
-    // initialaze an index
-    let index = 0;
-    // the index is the index of `hoverElement`
-    index = blurry.elements.indexOf(hoverElement);
-
-    for (let element of blurry.elements) {
-      // the index is stricly equal to `element`
-      if (index === element) {
-        // do nothing
-        return;
-      }
-      // `hoverElement` is not `element`
-      else if (hoverElement != element) {
-        // add the blurry CSS class to `element`
-        tools.addClassesToElement(element, "blurry");
-      }
-    }
+    blurry.setBlurOnOtherElements(event.currentTarget, true);
   },
   /**
-   * Removes a CSS class when the element is no longer overflow.
+   * Removes the blur from every element except the one that was being hovered.
    * @param {Event} event
    * @return {void}
    */
   handleMouseLeave: function (event) {
-    console.log("blurry.handleMouseLeave");
+    // console.log("blurry.handleMouseLeave()");
 
-    // get the DOM element form which the event occured
-    let hoverElement = event.currentTarget;
-
-    // initialaze an index
-    let index = 0;
-    // the index is the index of `hoverElement`
-    index = blurry.elements.indexOf(hoverElement);
+    blurry.setBlurOnOtherElements(event.currentTarget, false);
+  },
+  /**
+   * Adds or removes the "blurry" CSS class on every element of `blurry.elements` except `hoverElement`.
+   * @param {HTMLElement} hoverElement the element to leave untouched
+   * @param {Boolean} shouldBeBlurry true to add the class, false to remove it
+   * @return {void}
+   */
+  setBlurOnOtherElements: function (hoverElement, shouldBeBlurry) {
+    // console.log("blurry.setBlurOnOtherElements()");
 
     for (let element of blurry.elements) {
-      // the index is stricly equal to `element`
-      if (index === element) {
-        // do nothing
-        return;
-      }
-      // `hoverElement` is not `element`
-      else if (hoverElement != element) {
-        // remove the the blurry CSS class from `element`
+      // never blur the element the user is hovering
+      if (element === hoverElement) continue;
+
+      if (shouldBeBlurry) {
+        tools.addClassesToElement(element, "blurry");
+      } else {
         tools.removeClassesFromElement(element, "blurry");
       }
     }
